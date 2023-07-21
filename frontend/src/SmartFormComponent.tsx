8ff32d862f4c128b9ba2425ba8117d232e3fc19a
bsniegowski <sniegowski.beniamin@gmail.com>
2023-07-21 10:27:32 +0200

    After clicking 'edit' button the form is pre-filled with data

--- frontend/src/SmartFormComponent.tsx
+++ frontend/src/SmartFormComponent.tsx
@@ -2,7 +2,7 @@ import {Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField} fr
 import React, {useEffect, useState} from "react";
 import {BACKEND_BASE_URL} from "./config.ts";
 import LinearProgress from "@mui/material/LinearProgress";
-import {Formik, Form, Field, ErrorMessage, FormikConfig, FormikValues} from 'formik';
+import {Formik, Form, Field, FormikValues} from 'formik';
 
 interface SmartFormComponentProps {
   open: boolean
@@ -10,22 +10,37 @@ interface SmartFormComponentProps {
   formType: string
   rawData?: any
   onClose: any
-  id?: number
+  editId?: number
 }
 
 export const SmartFormComponent: React.FC<SmartFormComponentProps> =
   (props: SmartFormComponentProps) => {
     const [schema, setSchema] = useState<object>({})
     const [loading, setLoading] = useState<boolean>(true)
-    const [initialValues, setInitialValues] = useState<FormikValues>({})
+    const [initialValues, setInitialValues] = useState({})
+
+    const schemaUrl: string = `${BACKEND_BASE_URL}/${props.topic}/schema/${props.formType}`
+    const editDataUrl: string = `${BACKEND_BASE_URL}/${props.topic}/get?id=${props.editId}`
 
     useEffect(() => {
-      fetch(`${BACKEND_BASE_URL}/${props.topic}/schema/${props.formType}`)
+      fetch(schemaUrl)
         .then(response => response.json())
         .then(data => {
-          console.log(data)
+          console.log("schema", data)
           setSchema(data)
-          setLoading(false)
+          if (props.formType == "update") {
+            fetch(editDataUrl)
+              .then(response => response.json())
+              .then(data => {
+                console.log("editData[0]", data[0])
+                setInitialValues(data[0])
+                setLoading(false)
+              })
+              .catch(error => {
+                console.error('Error:', error)
+                setLoading(false)
+              })
+          } else setLoading(false)
         })
         .catch(error => {
           console.error('Error:', error)
@@ -40,23 +55,29 @@ export const SmartFormComponent: React.FC<SmartFormComponentProps> =
 
     if (loading) return <LinearProgress/>
 
+    // @ts-ignore
     return !schema ? (<div>Error: fetched empty schema for chosen action</div>) : (
       <Dialog open={props.open} onClose={props.onClose}>
         <DialogTitle>Form type: {props.formType}</DialogTitle>
         <DialogContent>
           <Formik initialValues={initialValues} onSubmit={handleSubmit}>
             <Form>
-              {Object.keys(schema.properties).map((prop: string) => {
-                let property = schema.properties[prop]
-                return <Field
-                  as={TextField}
-                  key={property.title}
-                  type={property.type ?? "text"}
-                  label={property.title}
-                  variant="outlined"
-                  style={{margin: '10px'}}
-                />
-              })}
+              {
+                // @ts-ignore
+                Object.keys(schema.properties).map((prop: string) => {
+                  // @ts-ignore
+                  let property = schema.properties[prop]
+                  console.log(property.title)
+                  return <Field
+                    as={TextField}
+                    name={prop}
+                    type={property.type ?? "text"}
+                    label={property.title}
+                    variant="outlined"
+                    style={{margin: '10px'}}
+                  />
+                })
+              }
             </Form>
           </Formik>
         </DialogContent>
