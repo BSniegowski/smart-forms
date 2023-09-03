b8870667a0bcefa4f8d46814bfbdec445a8d667c
bsniegowski <sniegowski.beniamin@gmail.com>
2023-09-03 17:45:25 +0200

    Implemented database migrations using alembic

--- backend/models/machineTable.py
+++ backend/models/machineTable.py
@@ -14,3 +14,4 @@ class Machine(SQLModel, table=True):
     created_at: datetime = Field(default_factory=datetime.now)
     edited_at: datetime = Field(default_factory=datetime.now)
     password: str
+
